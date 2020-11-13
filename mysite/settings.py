ca02efc2859a11543e60ac8a9d4e9896af02a98b
Nikodem Wrosz <wrosznikodem@gmail.com>
2020-11-13 17:39:06 +0100

    change static settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,11 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'blog/static/'
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'blog/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 # To coś psuje:
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
